2ecb9889361516cec8041ed81b21d095539b6b2c
deividasbardzenis <deividasbarzdenis@yahoo.com>
2021-07-15 22:10:07 +0200

    Spring Security Example: JDBC-based user store

--- src/main/java/lt/debarz/tacocloud/security/SecurityConfig.java
+++ src/main/java/lt/debarz/tacocloud/security/SecurityConfig.java
@@ -1,20 +1,38 @@
 package lt.debarz.tacocloud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception{
+            throws Exception {
 
-        /*In-memory user store*/
+        /* 2. --- JDBC-based user store --- */
         auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username, password, enabled from Users " +
+                                "where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username, authority from UserAuthorities " +
+                                "where username=?")
+                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+        /* 1 ---- In-memory user store -----*/
+        /*auth
                 .inMemoryAuthentication()
                 .withUser("buzz")
                 .password("infinity")
@@ -22,7 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("woody")
                 .password("bullseye")
-                .authorities("ROLE_USER");
+                .authorities("ROLE_USER");*/
+
 
     }
 }

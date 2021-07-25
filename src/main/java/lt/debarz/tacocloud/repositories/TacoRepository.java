cf1ab74688853e5845fc67ed1ea328763dd1dd90
deividasbardzenis <deividasbarzdenis@yahoo.com>
2021-07-25 23:14:15 +0200

    Added chapter 6 from original repository.

--- src/main/java/lt/debarz/tacocloud/repositories/TacoRepository.java
+++
@@ -1,9 +0,0 @@
-package lt.debarz.tacocloud.repositories;
-
-import lt.debarz.tacocloud.entities.Taco;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
-
-}
